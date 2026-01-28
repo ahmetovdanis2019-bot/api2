b88af6ee30e9b04cda4956b79422a0bf7abac15d
Emma Paris <eparis@atlassian.com>
2026-01-28 22:17:14 +0500

    Add PostmanEchoTest

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+    import org.junit.jupiter.api.Test;
+    import static io.restassured.RestAssured.given;
+    import static org.hamcrest.Matchers.equalTo;
+
+    public class PostmanEchoTest {
+
+        @Test
+        void shouldReturnPostData() {
+            // Given - When - Then
+            given() // Предусловия
+                    .baseUri("https://postman-echo.com")
+                    .body("some data")
+                    .when()
+                    .post("/post")
+                    .then()
+                    .statusCode(200) 
+                    .body("data", equalTo("some data"))
+            ;
+        }
+    }
